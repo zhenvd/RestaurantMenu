85d92c5490ee7c9ecc9068e1f67d1cb510b45610
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-06-28 14:36:25 -0400

    first commit

---
+++ Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
